b19976b716d8caf734164d5314eea5173b386fe2
leksandrFo <leksandr.fo@gmail.com>
2023-04-29 13:26:19 +0300

    Added parser

---
+++ src/parser.js
@@ -0,0 +1,26 @@
+export default (data) => {
+  const parser = new DOMParser();
+  const parsedData = parser.parseFromString(data, 'application/xml');
+  const parseError = parsedData.querySelector("parsererror");
+  if (parseError) {
+    throw new Error(parseError);
+  }
+
+  const title = parsedData.documentElement.querySelector('title').textContent;
+  const description = parsedData.documentElement.querySelector('description').textContent;
+  const link = parsedData.documentElement.querySelector('link').textContent;
+  const feed = { title, description, link };
+
+  const items = Array.from(parsedData.querySelectorAll('item'));
+  const posts = items.map((item) => {
+    const postTitle = item.querySelector('title').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    const postLink = item.querySelector('link').textContent;
+    return {
+      title: postTitle,
+      description: postDescription,
+      link: postLink,
+    };
+  })
+  return { feed, posts };
+};
